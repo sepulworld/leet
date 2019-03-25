72655fec467c881f1263cb018921c8f72b013043
Zane Williamson <zanew@zillow.com>
2019-03-25 13:52:11 -0700

    adding robot origin

---
+++ geometry/robot_origin.py
@@ -0,0 +1,22 @@
+import pytest
+
+def judgeCircle(moves):
+    position = { "x": 0, "y": 0}
+    
+    for i in moves:
+        if i == "R":
+            position["y"] = position["y"] + 1
+        if i == "L":
+            position["y"] = position["y"] - 1
+        if i == "U":
+            position["x"] = position["x"] + 1
+        if i == "D":
+            position["x"] = position["x"] - 1
+    
+    if position == { "x": 0, "y": 0}:
+        return True
+    else:
+        return False
+
+assert judgeCircle("UD") == True 
+assert judgeCircle("UDLL") == False
